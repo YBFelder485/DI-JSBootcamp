a42371f684b3af0fa078c0fc363f792123072f31
Yaakov Felder <jbfelder8@gmail.com>
2023-01-23 15:04:47 +0200

    day2 exercises

--- Week2/Day1/dailyChallenge/exercise2/script.js
+++ Week2/Day1/dailyChallenge/exercise2/script.js
@@ -4,3 +4,4 @@
 const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
 
 console.log(moreFruits[1][1][0])
+
---
+++ Week2/Day2/exerciseXP/exercise1/script.js
@@ -0,0 +1,11 @@
+// Create 2 variables, x and y. Each of them should have a different numeric value.
+// Write an if/else statement that checks which number is bigger.
+
+const x = 18
+const y = 26
+
+if(x > y){
+    console.log("x is greater")
+} else {
+    console.log("y is the bigger number")
+}
