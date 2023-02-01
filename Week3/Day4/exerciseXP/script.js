e68a3883b0c5572bac837d0c83e6073a59f7671d
Yaakov Felder <jbfelder8@gmail.com>
2023-02-01 23:31:03 +0200

    exerciseXP

---
+++ Week3/Day4/exerciseXP/script.js
@@ -0,0 +1,62 @@
+generateColors()
+let currentColor
+listenToMain()
+let isMousePressed = false
+const button = document.querySelector(".clear button")
+listenToClearButton()
+
+
+function listenToClearButton(){
+    button.addEventListener("click", clearCanvas)
+}
+
+function clearCanvas(){
+    currentColor = null
+    const squares = document.querySelectorAll("#main > div")
+    for (const square of squares){
+        square.style.backgroundColor = "white"
+    }
+}
+
+function listenToMain (){
+    const main = document.getElementById("main")
+    main.addEventListener("mousedown", handleClick)
+    main.addEventListener("mousemove", handleMove)
+    document.body.addEventListener("mouseup", handleMouseRelease)
+}
+
+function handleMouseRelease(){
+    isMousePressed = false
+}
+
+function handleClick(){
+    if(currentColor == null) return
+    isMousePressed = true
+}
+
+function handleMove(e){
+    if(!isMousePressed) return
+    const hoveredDiv = document.elementFromPoint(e.x, e.y)
+    hoveredDiv.style.backgroundColor = currentColor
+}
+
+function generateColors(){
+    const colorDivs = document.querySelectorAll(".color")
+    for(const colorDiv of colorDivs){
+        const randomColor = generateRandomColor()
+        colorDiv.style.backgroundColor = randomColor
+        colorDiv.addEventListener("click", setCurrentColor)
+        colorDiv.dataset.color = randomColor
+    }
+}
+
+function generateRandomColor(){
+    const hex = Math.floor(Math.random() * 16777215).toString(16)
+    return "#" + hex
+}
+
+function setCurrentColor(e){
+    currentColor = e.target.dataset.color
+    currentColor = chosenColor
+    button.style.backgroundColor = chosenColor
+}
