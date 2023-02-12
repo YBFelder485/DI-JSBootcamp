6b2a18dc85ed9816775c03fd6251bdafacb96403
Yaakov Felder <jbfelder8@gmail.com>
2023-02-12 22:41:21 +0200

    exerciseXP

---
+++ Week5/Day1/exerciseXP/exercise1/script.js
@@ -0,0 +1,9 @@
+// Create a form like the one above. The form should contain three inputs:
+// a small text input asking for a name,
+// a larger textarea input to write a message,
+// a submit input (“Send”)
+
+// When you click the Send button, the form will be submitted with a GET method. (you can use the same HTML file for the action url)
+// Where will the sent data appear?
+
+//The data will appear in the URL
---
+++ Week5/Day1/exerciseXP/exercise2/script.js
@@ -0,0 +1,5 @@
+// Use the same form structure as Exercise 1.
+// When you click the Send button, the form will be submitted with a POST method.(you can use the same HTML file for the action url)
+// Where will the sent data appear? Hint : Look at the Network Tab
+
+//The data will appear under form data in the payload tab
