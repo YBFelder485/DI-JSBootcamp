de09fc80fb2cab634a52504e54653b8496a685a9
Yaakov Felder <jbfelder8@gmail.com>
2023-02-13 17:11:52 +0200

    exerciseXP

---
+++ Week5/Day2/exerciseXP/exercise2/script.js
@@ -0,0 +1,11 @@
+const url = "https://api.giphy.com/v1/gifs/search?q=sun&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My&limit=10&offset=2";
+
+const xhr = new XMLHttpRequest();
+xhr.addEventListener("load", logRequest);
+xhr.open("GET", url);
+xhr.send()
+
+function logRequest (e){
+    const response = JSON.parse(xhr.response);
+    console.log(response);
+}
